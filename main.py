598bc73a19552503a56e695b0e608c9255d3146d
Your Name <you@example.com>
2022-01-07 14:10:30 +0100

    first try

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import backtrader as bt
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class MyStrategy(bt.Strategy):
+    def next(self):
+        pass #Do something
+
+
+class PrintClose(bt.Strategy):
+    def __init__(self):
+        #Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Instantiate Cerebro engine
+    cerebro = bt.Cerebro()
+
+    # Add strategy to Cerebro
+    cerebro.addstrategy(PrintClose)
+
+    # Run Cerebro Engine
+    cerebro.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
